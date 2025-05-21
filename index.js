82ab5513e6587249e62adc227c9283ccbb2eff02
ThePhysiologist <63960141+ThePhysiologist@users.noreply.github.com>
2025-05-21 11:12:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ await page.goto(SCREENSHOT_URL, {
 
 
     // 5 second wait after page load
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await new Promise(resolve => setTimeout(resolve, 10000));
 
     await page.screenshot({ path: SCREENSHOT_PATH });
     await browser.close();
