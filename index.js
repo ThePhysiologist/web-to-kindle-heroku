50e7d5175a7cbec67b472e67290faa4230b9f2a0
ThePhysiologist <63960141+ThePhysiologist@users.noreply.github.com>
2025-05-21 09:31:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function convert(filename) {
 takeScreenshot();
 
 // Optionally re-run every X minutes
-setInterval(takeScreenshot, 20 * 60 * 1000); // every 30 minutes
+setInterval(takeScreenshot, 15 * 60 * 1000); // every 15 minutes
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
